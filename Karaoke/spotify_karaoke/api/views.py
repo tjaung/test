7fd55a3fa1de115996a9e970e1b9f099828e5303
tjaung <jaungt@gmail.com>
2022-03-15 18:12:12 -0400

    First commit

---
+++ Karaoke/spotify_karaoke/api/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework import generics, status
+from .serializers import RoomSerializer
+from .models import Room
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+# Create your views here.
+class RoomView(generics.CreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+    
